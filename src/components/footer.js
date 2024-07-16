bcdc849133d8d6f13b2eab593d2fff7f742fa681
ASH0DANNY <ash22714497@gmail.com>
2024-07-16 11:52:51 +0530

    .

--- src/components/footer.js
+++ src/components/footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
         </Typography>
         <Typography variant="body2" color="#fff" align="center">
           {/* Add your footer links here */}
-          <Link to="/" style={linkStyle}>HOME</Link> {" | "}
+          <Link to="/home" style={linkStyle}>HOME</Link> {" | "}
           <Link to="/history" style={linkStyle}>HISTORY</Link>
         </Typography>
       </Box>
