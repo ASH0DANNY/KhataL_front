0c10bdab5c3c45f0bd4c4760b1c89cb9eb5f66ac
ASH0DANNY <ash22714497@gmail.com>
2024-07-16 11:07:55 +0530

    .

--- src/components/cards/vendorCard.jsx
+++ src/components/cards/vendorCard.jsx
@@ -9,7 +9,7 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ErrorAlert, SuccessAlert } from "../utils/alerts";
 import SingleHistory from "./singleHistory";
 import { BASE_URL } from "../../helper";
@@ -36,13 +36,13 @@ const VendorCard = ({ vendor }) => {
   const cardRoot = {
     width: "100%",
     marginTop: "20px",
-    height: historyBody ? "90vh" : "55vh" ,
+    height: historyBody ? "90vh" : "55vh",
   };
 
   const handleUpdateBtn = async (vendorId) => {
     console.log("vendorId" + vendorId);
 
-    if (!Transtype || !transDesc ) {
+    if (!Transtype || !transDesc) {
       setAlertOpen(true);
 
       setTimeout(() => {
@@ -59,14 +59,11 @@ const VendorCard = ({ vendor }) => {
     };
 
     try {
-      const response = await fetch(
-        `${BASE_URL}/api/khata/vendor/${vendorId}`,
-        {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(vendorTransBody),
-        }
-      );
+      const response = await fetch(`${BASE_URL}/api/khata/vendor/${vendorId}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(vendorTransBody),
+      });
 
       if (response) {
         console.log("Transaction Updated : " + response.json());
@@ -74,34 +71,6 @@ const VendorCard = ({ vendor }) => {
     } catch (error) {
       console.error("Error updating balance:", error);
     }
-
-    useEffect(async() => {
-    //Updating My Balance
-    // const UpdateMyBalance = async ({ newTransAmount }) => {
-    try {
-      const response = await fetch(
-        `${BASE_URL}/api/khata/mybalance`,
-        {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            newBalance: transAmount,
-            transType: Transtype,
-          }),
-        }
-      );
-
-      if (response) {
-        console.log("Balance Updated  " + response.json().balance);
-      }
-    } catch (error) {
-      console.error("Error updating balance:", error);
-    }
-
-    
-      
-    }, []);
-    // };
 
     setupdateAlert(true);
     setTimeout(() => {
@@ -111,13 +80,33 @@ const VendorCard = ({ vendor }) => {
     }, 2000);
   };
 
+  useEffect(async () => {
+    //Updating My Balance
+    // const UpdateMyBalance = async ({ newTransAmount }) => {
+    try {
+      const response = await fetch(`${BASE_URL}/api/khata/mybalance`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          newBalance: transAmount,
+          transType: Transtype,
+        }),
+      });
+
+      if (response) {
+        console.log("Balance Updated  " + response.json().balance);
+      }
+    } catch (error) {
+      console.error("Error updating balance:", error);
+    }
+  }, []);
+  // };
+
   const getSingleHistory = async (vendorId) => {
     historyBody ? setHistoryBody(false) : setHistoryBody(true);
 
     try {
-      const response = await fetch(
-        `${BASE_URL}/api/khata/vendor/${vendorId}`
-      );
+      const response = await fetch(`${BASE_URL}/api/khata/vendor/${vendorId}`);
 
       if (response) {
         const data = await response.json();
